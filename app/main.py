671bce11e9fca2782d0500a953310018e552ffd0
JesusTM <jesustapiamartin@gmail.com>
2025-10-30 14:56:19 -0300

    feat: agregar frontend web estático
    - Se agrega un frontend básico hecho en HTML con CSS y Bootstrap 5.
    - Aún no se integra con el backend

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI(title="HQC-MDD")
+
+app.mount("/", StaticFiles(directory="app/web/static/", html=True), name="static")
 
 # app.include_router(xml.router, prefix="/xml")
